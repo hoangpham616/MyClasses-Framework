850541eb25ad850a18b29a8e9cd808f31dc11f24
Hoàng Phạm <hoangpham61691@gmail.com>
2023-05-08 13:18:18 +0700

    * MyScreenshotEditorWindow

--- Assets/MyClasses/Sources/Scripts/Editor/MyScreenshotEditorWindow.cs
+++ Assets/MyClasses/Sources/Scripts/Editor/MyScreenshotEditorWindow.cs
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 Phạm Minh Hoàng
  * Email:       hoangpham61691@gmail.com
  * Framework:   MyClasses
- * Class:       MyScreenshotEditorWindow (version 1.0)
+ * Class:       MyScreenshotEditorWindow (version 1.1)
  */
 
 using UnityEditor;
@@ -41,7 +41,7 @@ namespace MyClasses.Tool
             titleContent = new GUIContent("[MyClasses] Screenshot");
             minSize = new Vector2(512, 768);
 
-            _directory = PlayerPrefs.GetString(KEY_DIRECTORY, "Assets/Screenshot/");
+            _directory = PlayerPrefs.GetString(KEY_DIRECTORY, "Assets/Screenshots/");
             _superSize = PlayerPrefs.GetInt(KEY_SUPER_SIZE, 1);
         }
 
@@ -61,6 +61,26 @@ namespace MyClasses.Tool
         {
             EditorGUILayout.BeginVertical();
 
+            if (GUILayout.Button("Frame Rate 30", GUILayout.Width(400)))
+            {
+                Application.targetFrameRate = 30;
+            }
+            if (GUILayout.Button("Frame Rate 60", GUILayout.Width(400)))
+            {
+                Application.targetFrameRate = 60;
+            }
+            if (GUILayout.Button("Frame Rate 120", GUILayout.Width(400)))
+            {
+                Application.targetFrameRate = 120;
+            }
+            if (GUILayout.Button("Frame Rate Max", GUILayout.Width(400)))
+            {
+                Application.targetFrameRate = -1;
+            }
+
+            EditorGUILayout.LabelField(string.Empty);
+            EditorGUILayout.LabelField("-----------------------------------------------------------------------------------", GUILayout.MaxWidth(400));
+            EditorGUILayout.LabelField(string.Empty);
             _directory = EditorGUILayout.TextField("Directory", _directory, GUILayout.Width(400));
             if (!_directory.EndsWith("/"))
             {
@@ -73,9 +93,12 @@ namespace MyClasses.Tool
             _superSize = EditorGUILayout.IntField("Super Size", _superSize, GUILayout.Width(400));
             _resolution = UnityEditor.Handles.GetMainGameViewSize();
             _size = _resolution * _superSize;
+            EditorGUILayout.BeginHorizontal();
             EditorGUILayout.LabelField("Resolution=" + _resolution.x + "x" + _resolution.y);
             EditorGUILayout.LabelField("Size=" + _size.x + "x" + _size.y);
-            if (GUILayout.Button("Take a Screenshot (Space bar)", GUILayout.Width(400)) 
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.LabelField(string.Empty);
+            if (GUILayout.Button("Take a Screenshot (press Space Bar)", GUILayout.Width(400)) 
                 || (Event.current.type == EventType.KeyUp && Event.current.keyCode == KeyCode.Space))
             {
                 if (!Directory.Exists(_directory))
@@ -88,10 +111,12 @@ namespace MyClasses.Tool
 
                 string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH'-'mm'-'ss");
                 _lastPath = _directory + currentTime + ".png";
-                RenderTexture renderTexture = RenderTexture.GetTemporary((int)_size.x, (int)_size.y, 32, RenderTextureFormat.Default, RenderTextureReadWrite.Default);
-                RenderTexture.active = renderTexture;
                 ScreenCapture.CaptureScreenshot(_lastPath, _superSize);
             }
+
+            EditorGUILayout.LabelField(string.Empty);
+            EditorGUILayout.LabelField("-----------------------------------------------------------------------------------", GUILayout.MaxWidth(400));
+            EditorGUILayout.LabelField(string.Empty);
             if (GUILayout.Button("Refresh Editor", GUILayout.Width(400)))
             {
                 AssetDatabase.Refresh();
@@ -100,7 +125,7 @@ namespace MyClasses.Tool
             if (_lastPath != null)
             {
                 EditorGUILayout.LabelField(string.Empty);
-                EditorGUILayout.LabelField("___________________________________________________________________________________", GUILayout.MaxWidth(400));
+                EditorGUILayout.LabelField("-----------------------------------------------------------------------------------", GUILayout.MaxWidth(400));
                 EditorGUILayout.LabelField(string.Empty);
                 GUIStyle styleCenter = new GUIStyle(GUI.skin.label);
                 styleCenter.alignment = TextAnchor.MiddleCenter;
@@ -124,31 +149,6 @@ namespace MyClasses.Tool
             }
 
             EditorGUILayout.EndVertical();
-        }
-
-        #endregion
-
-        #region ----- Private Method -----
-
-        /// <summary>
-        /// Take a screenshot.
-        /// </summary>
-        private void _CaptureScreenshot()
-        {
-            if (!Directory.Exists(_directory))
-            {
-                Directory.CreateDirectory(_directory);
-            }
-
-            PlayerPrefs.SetString(KEY_DIRECTORY, _directory);
-            PlayerPrefs.SetFloat(KEY_SUPER_SIZE, _superSize);
-
-            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH'-'mm'-'ss");
-            _lastPath = _directory + currentTime + ".png";
-            RenderTexture renderTexture = RenderTexture.GetTemporary((int)_size.x, (int)_size.y, 32, RenderTextureFormat.Default, RenderTextureReadWrite.Default);
-            RenderTexture.active = renderTexture;
-            ScreenCapture.CaptureScreenshot(_lastPath, _superSize);
-            AssetDatabase.Refresh();
         }
 
         #endregion
